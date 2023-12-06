91129b0c1150e4ec4dc0ed62e018b5f7e6008cf4
Morgan <morgan.espindola@gmail.com>
2023-12-05 23:30:41 -0300

    Chapter 05 (Hash) - Book

---
+++ chapter 05/book.py
@@ -0,0 +1,9 @@
+book = dict()
+
+book["apple"] = 0.67
+book["milk"] = 1.49
+book["avocado"] = 2.03
+book["banana"] = 0.99
+
+print("Full Book = " + str(book))
+print("Avocado Price = " + str(book["avocado"]))
