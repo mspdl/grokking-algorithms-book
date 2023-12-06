58291cc08da8acd50cf7cbaf93184334dd8006e3
Morgan <morgan.espindola@gmail.com>
2023-12-05 23:33:31 -0300

    Chapter 05 (Hash) - Phone Book

---
+++ chapter 05/phone_book.py
@@ -0,0 +1,7 @@
+phone_book = {}
+
+phone_book["jenny"] = 8675309
+phone_book["emergency"] = 911
+
+print("Phone Book = " + str(phone_book))
+print("Emergency Number = " + str(phone_book["emergency"]))
