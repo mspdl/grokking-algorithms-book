16b85c99cab93d6a31671524d5cdaa89eb13191a
Morgan <morgan.espindola@gmail.com>
2023-12-07 22:11:47 -0300

    Chapter 06 (Graphs) - Search the mango seller

--- chapter 06/first_graph.py
+++ chapter 06/first_graph.py
@@ -2,11 +2,14 @@ graph = {}
 graph["you"] = ["alice", "bob", "claire"]
 graph["bob"] = ["anuj", "peggy"]
 graph["alice"] = ["peggy"]
-graph["thom"] = ["jonny"]
+graph["claire"] = ["thom", "jonny"]
 graph["anuj"] = []
 graph["peggy"] = []
 graph["thom"] = []
 graph["jonny"] = []
+
+search_queue = deque()
+search_queue += graph["you"]
 
 print("All graphs = " + str(graph))
 print("Your vertices = " + str(graph["you"]))
